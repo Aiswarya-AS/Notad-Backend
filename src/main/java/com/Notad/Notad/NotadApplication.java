2418338878b01f31040a5da5940494056dc51542
Aiswarya-AS <aiswaryaasubash@gmail.com>
2024-03-04 22:57:44 +0530

    initial commit

---
+++ src/main/java/com/Notad/Notad/NotadApplication.java
@@ -0,0 +1,13 @@
+package com.Notad.Notad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Notad/Notad/NotadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Notad.Notad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
