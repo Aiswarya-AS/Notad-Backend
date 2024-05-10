39d4fd9a7a36f3bc46a830836d3f7c1568378652
Aiswarya AS <83812425+Aiswarya-AS@users.noreply.github.com>
2024-05-10 23:35:24 +0530

    Database setup, User crud (#1)
    * psql db setup and migration
    
    * user table added
    
    * user crud

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from core.config import settings
+from db.session import engine 
+from db.base import Base
+from api.base import api_router
 
-app = FastAPI()
+def create_tables():         
+	Base.metadata.create_all(bind=engine)
+        
+def include_router(app):   
+	app.include_router(api_router)
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+    create_tables()
+    include_router(app)
+    return app
+
+
+app = start_application()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def home():
+    return {"msg":"Hello FastAPI🚀"}
+
