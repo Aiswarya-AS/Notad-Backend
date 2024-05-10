746c7c3481a76599dac2ad1d4e8fe6842e1090dd
Aiswarya-AS <aiswaryaasubash@gmail.com>
2024-05-10 20:33:08 +0530

    user table added

---
+++ db/models/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Integer, String, ForeignKey, Column, Boolean
+from db.base_class import Base
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+
+
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str]
+    email: Mapped[str] = mapped_column(String(30))
+    password: Mapped[str] = mapped_column(String(30))
+    is_superuser: Mapped[bool] = Column(Boolean)
+    is_active: Mapped[bool] = Column(Boolean)
